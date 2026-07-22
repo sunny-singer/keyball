9e1eccfe80d0bb082a8a878e03c68caae5615395
Sunny Singer <sunny.singer997@proton.me>
2026-07-22 15:27:18 +0900

    change point & scroll axes

--- qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
+++ qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
@@ -170,8 +170,8 @@ void pointing_device_driver_set_cpi(uint16_t cpi) {
 
 __attribute__((weak)) void keyball_on_apply_motion_to_mouse_move(keyball_motion_t *m, report_mouse_t *r, bool is_left) {
 #if KEYBALL_MODEL == 61 || KEYBALL_MODEL == 39 || KEYBALL_MODEL == 147 || KEYBALL_MODEL == 44
-    r->x = clip2int8(m->y);
-    r->y = clip2int8(m->x);
+    r->y = -clip2int8(m->y);
+    r->x = clip2int8(m->x);
     if (is_left) {
         r->x = -r->x;
         r->y = -r->y;
@@ -195,8 +195,8 @@ __attribute__((weak)) void keyball_on_apply_motion_to_mouse_scroll(keyball_motio
 
     // apply to mouse report.
 #if KEYBALL_MODEL == 61 || KEYBALL_MODEL == 39 || KEYBALL_MODEL == 147 || KEYBALL_MODEL == 44
-    r->h = clip2int8(y);
-    r->v = -clip2int8(x);
+    r->h = clip2int8(x);
+    r->v = -clip2int8(y);
     if (is_left) {
         r->h = -r->h;
         r->v = -r->v;
