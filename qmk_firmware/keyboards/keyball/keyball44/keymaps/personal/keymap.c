8bc21bea2b1c14044497cce38bb716f1a8f4d8e0
Sunny Singer <sunny.singer997@proton.me>
2026-08-08 20:51:08 +0900

    attempted fix for AML

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/personal/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/personal/keymap.c
@@ -47,9 +47,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default 
   // TODO set up navigation
   [navigation] = LAYOUT_universal(
-    DF(dvorak) , AML_TO , _______ , _______ , _______ , _______ ,                                       _______ , _______ , _______ , _______ , _______ , _______ ,
-    DF(qwerty) , _______ , _______ , _______ , _______ , _______ ,                                       _______ , KC_BTN1 , KC_BTN3 , _______ , KC_BTN2 , _______ ,
-    DF(gaming) , _______ , _______ , _______ , _______ , _______ ,                                       _______ , _______ , _______ , _______ , _______ , _______ ,
+    TO(dvorak) , AML_TO , _______ , _______ , _______ , _______ ,                                       _______ , _______ , _______ , _______ , _______ , _______ ,
+    TO(qwerty) , _______ , _______ , _______ , _______ , _______ ,                                       _______ , KC_BTN1 , KC_BTN3 , _______ , KC_BTN2 , _______ ,
+    TO(gaming) , _______ , _______ , _______ , _______ , _______ ,                                       _______ , _______ , _______ , _______ , _______ , _______ ,
              _______ , _______          , MO(navigation_mod) , _______ , _______ ,                  _______  , _______ , _______           , _______ , _______
   ),
 
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC   , KC_SCLN  , KC_COMM  , KC_DOT   , KC_P     , KC_Y     ,                                        KC_F     , KC_G     , KC_C     , KC_R     , KC_L     , _______  ,
     KC_TAB   , KC_A     , KC_O     , KC_E     , KC_U     , KC_I     ,                                        KC_D     , KC_H     , KC_T     , KC_N     , KC_S     , KC_MINS  ,
     KC_LSFT  , KC_QUOTE , KC_Q     , KC_J     , KC_K     , KC_X     ,                                        KC_B     , KC_M     , KC_W     , KC_V     , KC_Z     , KC_RSFT  ,
-               _______  , _______  , MO(dvorak_mod) , KC_SPC , _______ ,                          KC_BSPC  , KC_ENT   , _______  , _______  , DF(navigation)
+               _______  , _______  , MO(dvorak_mod) , KC_SPC , _______ ,                          KC_BSPC  , KC_ENT   , _______  , _______  , TO(navigation)
   ),
 
   [dvorak_mod] = LAYOUT_universal(
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , _______  ,
     KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , _______  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , _______  ,
-               _______  , _______  , MO(qwerty_mod)  , KC_SPC , _______ ,                          KC_BSPC , KC_ENT   , _______  , _______  , DF(navigation)
+               _______  , _______  , MO(qwerty_mod)  , KC_SPC , _______ ,                          KC_BSPC , KC_ENT   , _______  , _______  , TO(navigation)
   ),
 
   [qwerty_mod] = LAYOUT_universal(
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , _______  ,
     KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_BTN1  , KC_BTN3  , KC_BTN4  , KC_BTN2  , _______  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , _______  ,
-               _______  , _______  , MO(gaming_mod)  , KC_SPC , _______ ,                          KC_BSPC , KC_ENT   , _______  , _______  , DF(navigation)
+               _______  , _______  , MO(gaming_mod)  , KC_SPC , _______ ,                          KC_BSPC , KC_ENT   , _______  , _______  , TO(navigation)
   ),
 
   [gaming_mod] = LAYOUT_universal(
