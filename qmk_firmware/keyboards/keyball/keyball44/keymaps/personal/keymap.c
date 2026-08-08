536aa61f471c5d291a5d906e27a254dd0daf5b09
Sunny Singer <sunny.singer997@proton.me>
2026-08-08 20:18:23 +0900

    fixed mod names

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/personal/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/personal/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC   , KC_SCLN  , KC_COMM  , KC_DOT   , KC_P     , KC_Y     ,                                        KC_F     , KC_G     , KC_C     , KC_R     , KC_L     , _______  ,
     KC_TAB   , KC_A     , KC_O     , KC_E     , KC_U     , KC_I     ,                                        KC_D     , KC_H     , KC_T     , KC_N     , KC_S     , KC_MINS  ,
     KC_LSFT  , KC_QUOTE , KC_Q     , KC_J     , KC_K     , KC_X     ,                                        KC_B     , KC_M     , KC_W     , KC_V     , KC_Z     , KC_RSFT  ,
-               _______  , _______  , MO(programmer_dvorak_mod) , KC_SPC , _______ ,               KC_BSPC  , KC_ENT   , _______  , _______  , KC_CONFIG_NAVIGATION
+               _______  , _______  , MO(dvorak_mod) , KC_SPC , _______ ,               KC_BSPC  , KC_ENT   , _______  , _______  , KC_CONFIG_NAVIGATION
   ),
 
   [dvorak_mod] = LAYOUT_universal(
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , _______  ,
     KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_BTN1  , KC_BTN3  , KC_BTN4  , KC_BTN2  , _______  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , _______  ,
-               _______  , _______  , MO(qwerty_mod)  , KC_SPC , _______ ,                          KC_BSPC , KC_ENT   , _______  , _______  , KC_CONFIG_NAVIGATION
+               _______  , _______  , MO(gaming_mod)  , KC_SPC , _______ ,                          KC_BSPC , KC_ENT   , _______  , _______  , KC_CONFIG_NAVIGATION
   ),
 
   [gaming_mod] = LAYOUT_universal(
