988036518fd28064675e894695596ce6be92b389
Sunny Singer <sunny.singer997@proton.me>
2026-08-13 02:03:12 +0900

    LLCK 5

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/personal/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/personal/keymap.c
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 #include "quantum.h"
+#include "features/layer_lock.h"
 
 // DEVNOTE https://keymapdb.com/keymaps/jwon/
 // https://pseudocc.github.io/real-prog-dvorak/
--- qmk_firmware/keyboards/keyball/keyball44/keymaps/personal/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/personal/rules.mk
@@ -1,3 +1,5 @@
 RGBLIGHT_ENABLE = no
 
 OLED_ENABLE = yes
+
+SRC += features/layer_lock.c
