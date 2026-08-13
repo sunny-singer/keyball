e17f2bbac98ca1c6ade93993084f7dbbfcf3953e
Sunny Singer <sunny.singer997@proton.me>
2026-08-13 20:14:43 +0900

    fix include

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/personal/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/personal/keymap.c
@@ -19,7 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 #include "quantum.h"
-#include "features/layer_lock.h"
 
 // DEVNOTE https://keymapdb.com/keymaps/jwon/
 // https://pseudocc.github.io/real-prog-dvorak/
